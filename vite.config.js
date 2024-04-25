c11cf6272cec42351b62bb413dda2145daf0537d
kyupin64 <lewilks.321@gmail.com>
2024-04-25 07:50:59 -0600

    Added base to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://kyupin64.github.io/react-budget/",
 })
